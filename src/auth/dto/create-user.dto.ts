dc8e774e769d426f742910a0c53b2a956b042216
Franco Rodriguez <Franco.d.r1992@gmail.com>
2023-08-24 19:54:03 -0300

    first commit

---
+++ src/auth/dto/create-user.dto.ts
@@ -0,0 +1,20 @@
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
+
+
+export class CreateUserDto {
+
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    name: string;
+    
+    @MinLength(6)
+    password: string;
+
+    @IsString()
+    roles: string[];
+
+
+}
